40485a763528014c3639453bffa4954b42057d4c
cirjeudumitru-prog <cirjeu.dumitru@elev.cihcahul.md>
2026-09-15 10:27:23 +0300

    login server

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExceptionHandler } from '@nestjs/core/errors/exception-handler.js';
+import { NotFoundError } from 'rxjs';
 
 
 type AuthInput = { username: string, password: string };
 
-const users=[
+const users = [
     {
-        username:'admin1',
-        password:"fmn923jfahgnvbq43bv"
+        username: 'admin1',
+        password: "fmn923jfahgnvbq43bv"
     },
     {
-        username:'admin2',
-        password:"lcaomsciwqnvuebnvss"
+        username: 'admin2',
+        password: "lcaomsciwqnvuebnvss"
     },
     {
-        username:'admin3',
-        password:"plqwkafnbfsuwn"
+        username: 'admin3',
+        password: "plqwkafnbfsuwn"
     },
     {
-        username:'admin4',
-        password:"hdfqfhn9jdgsq4dgdf"
+        username: 'admin4',
+        password: "hdfqfhn9jdgsq4dgdf"
     },
 ]
 
@@ -27,6 +29,16 @@ const users=[
 export class AuthService {
 
     async login(input: AuthInput): Promise<any> {
+        const finduser = users.find(user => user.username === input.username)
+        if (!finduser) {
+            throw new UnauthorizedException("Nu exista user cu acest username")
+        }
+        if (finduser?.password !== input.password) {
+            throw new UnauthorizedException("Parola e gresita")
+        }
+
+        
+
 
     }
 
