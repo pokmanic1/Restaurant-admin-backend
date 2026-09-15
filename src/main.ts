429529699fbc1eb2c45ecec6ba8c078e9c1b2dd2
cirjeudumitru-prog <cirjeu.dumitru@elev.cihcahul.md>
2026-09-15 11:02:25 +0300

    interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { UtilsInterceptor } from './utils/utils.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalInterceptors(new UtilsInterceptor())
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
--- src/utils/utils.interceptor.ts
+++ src/utils/utils.interceptor.ts
@@ -1,9 +1,26 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable ,map } from 'rxjs';
 
 @Injectable()
-export class UtilsInterceptor implements NestInterceptor {
+export class UtilsInterceptor<T> implements NestInterceptor {
+  
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+
+    const response=context.switchToHttp().getResponse();
+    const statusCode=response.statusCode??200;
+
+
+
+
+
+
+    return next.handle().pipe(
+      map((data:T)=>({
+        statusCode,
+        message:'succees',
+        data
+      }))
+    )
+  
   }
 }
